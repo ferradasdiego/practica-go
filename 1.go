ce4f3653ff69d8e1fac000d44f88e9ea9b198f7a
ferradasdiego <32649684+ferradasdiego@users.noreply.github.com>
2022-10-04 10:22:53 +0200

    Add files via upload

--- 1.go
+++ 1.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+// No devuelve nada, pinta por consola el string y el int que le pasas
+func sayHello(name string, years int) {
+	var yearsToS string = strconv.Itoa(years)
+	fmt.Println("Hello, " + name + " you have " + yearsToS + " years old")
+}
+
+// Devuelve un string pasando un string y un int
+func sayHello2(name string, years int) string {
+	return ("Your name is: " + name + " and you have " + strconv.Itoa(years) + " old")
+}
+
+// Devuelve un string y un int
+func sayHello3() (string, int) {
+	return ("hola"), (5)
+}
+
+func main() {
+	sayHello("diego", 27)
+	fmt.Println(sayHello2("diego", 27))
+	fmt.Println(sayHello3())
+}
